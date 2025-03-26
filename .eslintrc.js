bc31c3efa0167879c03256e3d646391ca35e24cc
Kunal Jain <kunalmandot1999@gmail.com>
2025-03-26 09:46:38 +0530

    Updated eslintrc file

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,19 +14,5 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 'latest',
   },
-  rules: {
-    // 'max-len': [2, { code: 120 }],
-    // 'max-params': [2, 5],
-    // 'curly': [2, 'all'],
-    // 'quote-props': [2, 'consistent-as-needed'],
-    // 'no-console': 2,
-    // 'no-unused-vars': [2, { args: 'all', argsIgnorePattern: '^_' }],
-    // 'no-param-reassign': [2, { props: false }],
-    // 'import/no-extraneous-dependencies': [2, { devDependencies: true }],
-    // 'node/no-unpublished-require': [2, {
-    //   allowModules: [
-    //     'swagger-jsdoc', 'swagger-ui-express',
-    //   ],
-    // }],
-  },
+  rules: {},
 };
